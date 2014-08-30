3fb0b7637bbfab129a8ae91e045cd6e83da419e2
miyohide <miyohide@gmail.com>
2014-08-30 11:03:38 +0900

    initial commit

---
+++ rbs_bat.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+dstat -tcm -C 0,1,2,3,4,5,6,7 --output ruby_benchmark_suite_dstat_result.csv &
+
+cd ruby_benchmark_suite
+
+rbenv shell 2.1.2
+ruby -v
+date
+rake bench
+date
+
+sleep 600
+
+rbenv shell jruby-1.7.14
+ruby -v
+date
+rake bench
+date
+
+sleep 600
+
+rbenv shell rbx-2.2.10
+ruby -v
+date
+rake bench
+date
+
