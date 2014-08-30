1b9279447384f9527d9e6cd749dd33c3369f9e1c
miyohide <miyohide@gmail.com>
2014-08-30 11:17:21 +0900

    not use rbenv command.

--- rbs_bat.sh
+++ rbs_bat.sh
@@ -2,25 +2,29 @@
 
 dstat -tcm -C 0,1,2,3,4,5,6,7 --output ~/dstat_results/ruby_benchmark_suite_dstat_result.csv &
 
+echo "2.1.2" > ~/ruby-benchmark-suite/.ruby-version
 cd ~/ruby-benchmark-suite
 
-rbenv shell 2.1.2
 ruby -v
 date
 rake bench
 date
 
 sleep 600
+cd ~/
+echo "jruby-1.7.14" > ~/ruby-benchmark-suite/.ruby-version
+cd ~/ruby-benchmark-suite
 
-rbenv shell jruby-1.7.14
 ruby -v
 date
 rake bench
 date
 
 sleep 600
+cd ~/
+echo "rbx-2.2.10" > ~/ruby-benchmark-suite/.ruby-version
+cd ~/ruby-benchmark-suite
 
-rbenv shell rbx-2.2.10
 ruby -v
 date
 rake bench
